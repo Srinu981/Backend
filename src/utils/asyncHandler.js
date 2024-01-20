d286626c7db154b87b9c2f396e964e4b8d8f2c3b
srinu <srinu525225@gmail.com>
2024-01-20 11:47:44 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHanlder = (requestHandler) =>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
+
+
+
+
+
+export {asyncHanlder}
+
+
+// const syncHanlder = (fn) =>async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//       res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message
+//       })  
+//     }
+// }
